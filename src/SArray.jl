35391648f0f1f516f15e1ac2ee25ce1b0e7ea0b1
Jan Weidner <jw3126@gmail.com>
2017-09-21 20:18:23 +0200

    clear SArray.jl

--- src/SArray.jl
+++ src/SArray.jl
@@ -1,20 +1,3 @@
-"""
-    SArray{S, T, L}(x::NTuple{L, T})
-    SArray{S, T, L}(x1, x2, x3, ...)
-
-Construct a statically-sized array `SArray`. Since this type is immutable, the data must be
-provided upon construction and cannot be mutated later. The `S` parameter is a Tuple-type
-specifying the dimensions, or size, of the array - such as `Tuple{3,4,5}` for a 3×4×5-sized
-array. The `L` parameter is the `length` of the array and is always equal to `prod(S)`.
-Constructors may drop the `L` and `T` parameters if they are inferrable from the input
-(e.g. `L` is always inferrable from `S`).
-
-    SArray{S}(a::Array)
-
-Construct a statically-sized array of dimensions `S` (expressed as a `Tuple{...}`) using
-the data from `a`. The `S` parameter is mandatory since the size of `a` is unknown to the
-compiler (the element type may optionally also be specified).
-"""
 struct SArray{S <: Tuple, T, N, L} <: StaticArray{S, T, N}
     data::NTuple{L,T}
 
