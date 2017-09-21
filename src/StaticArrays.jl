74048d85a64be6c1d643e79ba4d082bc08ce1c9c
Jan Weidner <jw3126@gmail.com>
2017-09-21 20:22:30 +0200

    clear StaticArrays.jl

--- src/StaticArrays.jl
+++ src/StaticArrays.jl
@@ -29,35 +29,6 @@ export @MVector, @MMatrix, @MArray
 export similar_type
 export push, pop, shift, unshift, insert, deleteat, setindex
 
-"""
-    abstract type StaticArray{S, T, N} <: AbstractArray{T, N} end
-    StaticScalar{T}     = StaticArray{Tuple{}, T, 0}
-    StaticVector{N,T}   = StaticArray{Tuple{N}, T, 1}
-    StaticMatrix{N,M,T} = StaticArray{Tuple{N,M}, T, 2}
-
-`StaticArray`s are Julia arrays with fixed, known size.
-
-## Dev docs
-
-They must define the following methods:
- - Constructors that accept a flat tuple of data.
- - `getindex()` with an integer (linear indexing) (preferably `@inline` with `@boundscheck`).
- - `Tuple()`, returning the data in a flat Tuple.
-
-It may be useful to implement:
-
-- `similar_type(::Type{MyStaticArray}, ::Type{NewElType}, ::Size{NewSize})`, returning a
-  type (or type constructor) that accepts a flat tuple of data.
-
-For mutable containers you may also need to define the following:
-
- - `setindex!` for a single element (linear indexing).
- - `similar(::Type{MyStaticArray}, ::Type{NewElType}, ::Size{NewSize})`.
- - In some cases, a zero-parameter constructor, `MyStaticArray{...}()` for unintialized data
-   is assumed to exist.
-
-(see also `SVector`, `SMatrix`, `SArray`, `MVector`, `MMatrix`, `MArray`, `SizedArray` and `FieldVector`)
-"""
 abstract type StaticArray{S <: Tuple, T, N} <: AbstractArray{T, N} end
 const StaticScalar{T} = StaticArray{Tuple{}, T, 0}
 const StaticVector{N, T} = StaticArray{Tuple{N}, T, 1}
