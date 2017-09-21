47ef02e9b8ba26e175028af6fb6ee6c0a58e2da9
Jan Weidner <jw3126@gmail.com>
2017-09-21 20:24:36 +0200

    clear MArray.jl

--- src/MArray.jl
+++ src/MArray.jl
@@ -1,22 +1,3 @@
-"""
-    MArray{S, T, L}()
-    MArray{S, T, L}(x::NTuple{L, T})
-    MArray{S, T, L}(x1, x2, x3, ...)
-
-
-Construct a statically-sized, mutable array `MArray`. The data may optionally be
-provided upon construction and cannot be mutated later. The `S` parameter is a Tuple-type
-specifying the dimensions, or size, of the array - such as `Tuple{3,4,5}` for a 3×4×5-sized
-array. The `L` parameter is the `length` of the array and is always equal to `prod(S)`.
-Constructors may drop the `L` and `T` parameters if they are inferrable from the input
-(e.g. `L` is always inferrable from `S`).
-
-    MArray{S}(a::Array)
-
-Construct a statically-sized, mutable array of dimensions `S` (expressed as a `Tuple{...}`)
-using the data from `a`. The `S` parameter is mandatory since the size of `a` is unknown to
-the compiler (the element type may optionally also be specified).
-"""
 mutable struct MArray{S <: Tuple, T, N, L} <: StaticArray{S, T, N}
     data::NTuple{L,T}
 
