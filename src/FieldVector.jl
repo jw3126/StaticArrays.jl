5087aae6fdf943707a305c332150846037ec2ea1
Jan Weidner <jw3126@gmail.com>
2017-09-21 20:19:23 +0200

    clear Fieldvector.jl

--- src/FieldVector.jl
+++ src/FieldVector.jl
@@ -1,19 +1,3 @@
-"""
-    abstract FieldVector{N, T} <: StaticVector{N, T}
-
-Inheriting from this type will make it easy to create your own vector types. A `FieldVector`
-will automatically define `getindex` and `setindex!` appropriately. An immutable
-`FieldVector` will be as performant as an `SVector` of similar length and element type,
-while a mutable `FieldVector` will behave similarly to an `MVector`.
-
-For example:
-
-    struct Point3D <: FieldVector{3, Float64}
-        x::Float64
-        y::Float64
-        z::Float64
-    end
-"""
 abstract type FieldVector{N, T} <: StaticVector{N, T} end
 
 # Is this a good idea?? Should people just define constructors that accept tuples?
